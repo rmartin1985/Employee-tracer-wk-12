cfdf3bc7ba0f0f2affa20a6a943d01a94cd09d74
Rick Martin <rmartin1985@att.net>
2021-10-31 12:29:46 -0500

    used dotenv to hide sensitive info

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,14 @@
 // File to create the connection to mysql database
+const mysql = require('mysql2');
+
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME
+});
+
+module.exports = connection;
